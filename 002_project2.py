425b7f1ec5f8a8be6592228ae6c6e2c1ff4b07d0
opecloude_DevOps <opecloude@gmail.com>
2022-12-30 09:37:20 -0500

    PythonFile from VSCode

---
+++ 002_project2.py
@@ -0,0 +1,8 @@
+
+
+floatValue = " 12.43"
+isWord = 'This students are just too\x3A good this\r days'
+isList = [9,8,7,6,5,43,2,3,]
+#print(help('module'))
+#print(isWord.center())
+print(float(floatValue))
